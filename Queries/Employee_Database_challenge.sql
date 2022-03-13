f6af352b927014baf345289bd14fe6bd4f690adb
AktugChelekche <aktugcilekci@hotmail.com>
2022-03-13 15:51:33 -0500

    Exported Retiring and Unique tites CSVS

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,108 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+Select * From departments
+
+CREATE TABLE employees (
+     emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+Select * From employees
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+Select * From dept_manager
+
+
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+Select * From salaries
+
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+
+
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+select * from titles as t 
+select * from employees as e 
+select * from departments  as d
+select * from dept_emp as d_e
+select * from dept_manager as d_m
+select * from salaries as s
+
+Select e.emp_no , 
+e.first_name , 
+e.last_name,
+t.title,
+t.from_date,
+t.to_date 
+INTO retirement_titles 
+From employees as e 
+JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+select * from retirement_titles limit 100
+
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles 
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+select * from unique_titles limit 100 
+
+DROP TABLE retiring_titles
+SELECT COUNT(title) ,title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC
+
+select * from retiring_titles limit 10
--- schema.sql
+++ schema.sql
@@ -41,7 +41,7 @@ CREATE TABLE dept_emp (
     to_date DATE NOT NULL,
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no,dept_no)
 );
 
 CREATE TABLE titles (
