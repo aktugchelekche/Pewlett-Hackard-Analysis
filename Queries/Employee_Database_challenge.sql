1f853658331669024ad5506d423ed6341dd2a4bb
AktugChelekche <aktugcilekci@hotmail.com>
2022-03-14 16:32:42 -0500

    Readme updated

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,6 @@
 -- Creating tables for PH-EmployeeDB
+---------------------------------------------------------------------------------------------------------------------
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -71,6 +73,13 @@ select * from dept_emp as d_e
 select * from dept_manager as d_m
 select * from salaries as s
 
+
+--Deliverable 1: 
+---------------------------------------------------------------------------------------------------------------------
+
+-- 1. The Number of Retiring Employees by Title:
+
+
 Select e.emp_no , 
 e.first_name , 
 e.last_name,
@@ -86,6 +95,7 @@ order by e.emp_no;
 
 select * from retirement_titles limit 100
 
+--2. The Number of Retiring Employees by Title (Duplicates Removev BY DISTINCT ON()):
 
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -98,7 +108,8 @@ ORDER BY emp_no, to_date DESC;
 
 select * from unique_titles limit 100 
 
-DROP TABLE retiring_titles
+-- 3. The number of employees by their most recent  title  about to retire :
+
 SELECT COUNT(title) ,title 
 INTO retiring_titles
 FROM unique_titles
@@ -106,3 +117,63 @@ GROUP BY title
 ORDER BY COUNT(title) DESC
 
 select * from retiring_titles limit 10
+
+--Deliverable 2: 
+
+-----------------------------------------------------------------------------------------------------------------
+--1. The Employees Eligible for the Mentorship Program.
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no ,  
+e.first_name,
+e.last_name,
+e.birth_date,
+d_e.from_date,
+d_e.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees as e
+JOIN dept_emp as d_e 
+ON (e.emp_no = d_e.emp_no)
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (d_e.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+
+SELECT * FROM mentorship_eligibilty 
+
+--Supporting Tables for Deliverable 3 :
+----------------------------------------------------------------------------------------------------------
+
+-- 1. Roles per Staff and Departament (Retirement Titles table does not have DEPT_NAME)
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title,
+	d.dept_name
+INTO unique_per_department
+FROM retirement_titles as rt
+JOIN dept_emp as d_e
+ON (rt.emp_no = d_e.emp_no)
+JOIN departments as d
+ON (d.dept_no = d_e.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT * FROM unique_per_department limit 100
+
+
+-- 2.  Number of Specific Role needed per Department 
+SELECT dept_name, 
+title,
+COUNT(title) 
+INTO department_need
+FROM unique_per_department 
+GROUP BY title, dept_name
+ORDER BY dept_name DESC
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Pewlett-Hackard-Analysis
+# Pewlett Hackard Analysis
+
+## Overview of Project:
+
+Pewlett-Hackard is one of the largest company in its field and there is a upcoming "Silver Tsunami" in few years. They wanted to be ready for it so that an HR analysis Bobby was assigned for creating databases and my job is to help Bobby to build an Employee Database with SQL by utilizing my Data Modelling, Engineering, and Analysis skills. 
+
+
+#### Purpose of Pewlett Hackard Analysis :
+
+Our main purpose to answer two fundamental questions for Pewlett-Hackard  :
+* 1- Who will be retiring in next few years ?
+* 2- How many positions will Pewlett-Hackard need to fill ?
+
+To answers mentioned questions, Bobby and myself decided that we needed to create following datasets : 
+
+*The Number of Retiring Employees by Title:
+    ** Calculate the sum of retiring employees grouped by title
+    **Calculate the number of roles the company needs to fill by grouping title and department.
+*The Employees Eligible for the Mentorship Program:
+    ** Calculate number retiring employees to mentor new hired by grouping by title and department.
+    
+This analysis will also help future-proof Pewlett-Hackard by generating a list of all employees eligible for retirement package 
+
+
+## Analysis
+
+As first part of the analysis, we quickly scanned six datasets that was provided by the HR and identify the components that are vital for creating targeted databases:
+1- Primary Key for each dataset : That is a unique key for each dataset 
+2- Foreign Key for each dataset : That is a key to connect dataset to each other. 
+
+##### ERD and Schema :
+
+Next, we created an Entity Relation Diagram (ERDS) by using " Quick DBD " that will assist us to demonstrate varies relations between each and every datasets as in Image-1 .
+
+
+#####
+
+Lastly, we started creating databases by usiung provided datasets and utilizing PostGres for holding data and PgAdmin which is an interface  to communicate with P0stGres. 
+
